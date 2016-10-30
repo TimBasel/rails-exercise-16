31b7a78b7ba331a76904001f71da043e198fa11a
aloeser <alexander.loeser@student.hpi.de>
2016-10-30 18:40:10 +0000

    Failing Test for #3

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "authors/new.html.erb", type: :view do
     render :template => "authors/new.html.erb"
     expect(rendered).to have_field("First name")
   end
+
+  it "has field last name" do
+    render :template => "authors/new.html.erb"
+    expect(rendered).to have_field("Last name")
+  end
 end
