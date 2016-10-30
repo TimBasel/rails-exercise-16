430c3b99d2f68e799630750e6bf6b6534e71bec2
aloeser <alexander.loeser@student.hpi.de>
2016-10-30 11:26:55 +0000

    Added Test for #2

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
+require 'capybara/rspec'
 
 RSpec.describe "authors/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has field first name" do
+    render :template => "authors/new.html.erb"
+    expect(rendered).to have_field("first name")
+  end
 end
