49ca438c84f44d4e8b038070684e8dc2f59f8764
aloeser <alexander.loeser@student.hpi.de>
2016-10-30 18:33:49 +0000

    Failing test for #2

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
-require 'capybara/rspec'
 
 RSpec.describe "authors/new.html.erb", type: :view do
   it "has field first name" do
     render :template => "authors/new.html.erb"
-    expect(rendered).to have_field("first name")
+    expect(rendered).to have_field("First name")
   end
 end
