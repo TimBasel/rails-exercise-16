15f6be0863563b28d6186b94d58ba0cd4f20cdab
aloeser <alexander.loeser@student.hpi.de>
2016-11-04 10:21:10 +0000

    Failing Test for #14

--- spec/views/authors/show.html.erb_spec.rb
+++ spec/views/authors/show.html.erb_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe "authors/show.html.erb", type: :view do
     expect(rendered).to have_text("First name: Alan")
   end
 
+  it "shows the last name" do
+    assign(:author, build(:author))
+    render :template => "authors/show.html.erb"
+    expect(rendered).to have_text("Last name: Turing")
+  end
 end
