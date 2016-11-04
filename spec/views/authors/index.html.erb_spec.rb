e89a57354010cde3be35d47703e00c1205e89d29
aloeser <alexander.loeser@student.hpi.de>
2016-11-04 11:51:50 +0000

    Failing Test for #19

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe "authors/index.html.erb", type: :view do
     expect(rendered).to have_text("http://wikipedia.de/Alan_Turing")
   end
 
+  it "has new author link" do
+    create(:author)
+    assign(:authors, Author.all)
+    render :template => "authors/index.html.erb"
+    expect(rendered).to have_css("a", :text => "add author")
+  end
+
+    
 end
