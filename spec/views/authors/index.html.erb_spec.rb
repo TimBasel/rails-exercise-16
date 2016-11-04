cd50ae618c338d30f691cad3e04403d6280243e8
aloeser <alexander.loeser@student.hpi.de>
2016-11-04 12:27:53 +0000

    Fix #21 and refactored Test for #21 and previous issues

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -2,31 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "authors/index.html.erb", type: :view do
 
-  it "displays author name" do
+  before(:context) do
     create(:author)
     assign(:authors, Author.all)
-    render :template => "authors/index.html.erb"
+  end
+
+  before(:example) do
+    render :template => "authors/index.html.erb"        
+  end
+
+  it "displays author name" do
     expect(rendered).to have_text("Alan Turing")
   end
 
   it "displays author homepage" do
-    create(:author)
-    assign(:authors, Author.all)
-    render :template => "authors/index.html.erb"
     expect(rendered).to have_text("http://wikipedia.de/Alan_Turing")
   end
 
   it "has new author link" do
-    create(:author)
-    assign(:authors, Author.all)
-    render :template => "authors/index.html.erb"
     expect(rendered).to have_css("a", :text => "Add author")
   end
 
   it "links to the author page" do
-    create(:author)
-    assign(:authors, Author.all)
-    render :template => "authors/index.html.erb"
     expect(rendered).to have_css("a", :text => "Show")
   end    
 end
