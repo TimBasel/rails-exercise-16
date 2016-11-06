a5beefef0dd51d2b3217a6ff034c0e772a49641e
aloeser <alexander.loeser@student.hpi.de>
2016-11-06 10:55:00 +0000

    Fix #29

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -25,6 +25,10 @@ class AuthorsController < ApplicationController
         @author = Author.find(params[:id])
     end
 
+    def update
+
+    end
+
     private
         def author_params
             params.require(:author).permit(:first_name, :last_name, :homepage)
