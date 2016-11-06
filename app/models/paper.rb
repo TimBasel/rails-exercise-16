b5a47888a66d324ea729d5309ff38e8b91698d18
aloeser <alexander.loeser@student.hpi.de>
2016-11-06 16:03:00 +0000

    Fix #41

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,6 @@ class Paper < ActiveRecord::Base
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
 
+  has_and_belongs_to_many :authors
+
 end
