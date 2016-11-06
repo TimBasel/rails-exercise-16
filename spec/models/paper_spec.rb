a5dd4154908479b98b195b66e682deee32fdceda
aloeser <alexander.loeser@student.hpi.de>
2016-11-06 15:11:31 +0000

    Failing Test for #35

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "requires a title" do
+    @paper = Paper.new
+    expect(@paper).to_not be_valid
+  end
 end
