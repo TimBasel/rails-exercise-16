24d0465b64e196f81e2c40ed2cbadc59bb1245de
aloeser <alexander.loeser@student.hpi.de>
2016-11-06 15:21:17 +0000

    Failing Test for #37

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   
   it "requires a title" do
-    @paper = Paper.new(venue: "whatever")
+    @paper = Paper.new(venue: "whatever", year: 1337)
     expect(@paper).to_not be_valid
   end
 
   it "requires a venue" do
-    @paper = Paper.new(title: "whatever")
+    @paper = Paper.new(title: "whatever", year: 1337)
+    expect(@paper).to_not be_valid
+  end
+
+  it "requires a year" do
+    @paper = Paper.new(title: "whatever", venue: "1337")
     expect(@paper).to_not be_valid
   end
 end
